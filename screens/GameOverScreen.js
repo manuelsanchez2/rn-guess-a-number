b585ed8dbb88e82388afc549151fb5037752f4b0
manuelsanchez2 <manusansan22@gmail.com>
2020-10-23 15:19:24 +0200

    Create new play button

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import Colors from "../constants/colors";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Overrrrrr</Text>
+      <Text style={styles.title}>The Game is Over</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
+      <Text>It was clear your number was {props.userNumber}</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          color={Colors.contrast}
+          title="PLAY AGAIN"
+          onPress={props.onRestart}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+
+  buttonContainer: {
+    marginVertical: 20,
   },
 });
 
