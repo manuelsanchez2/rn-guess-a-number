a5fb2cdc75e62ab5e189d255522e5a83a2d7f71f
manuelsanchez2 <manusansan22@gmail.com>
2020-10-23 14:15:22 +0200

    Create a random number generator function

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
 const GameScreen = (props) => {};
 
 const styles = StyleSheet.create({});
