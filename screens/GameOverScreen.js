c6d6a559bcf1ae8bdd0a42c0dc9afff85263b4d7
manuelsanchez2 <manusansan22@gmail.com>
2020-10-23 15:40:53 +0200

    Create BodyText components

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import Colors from "../constants/colors";
 
+import BodyText from "../components/BodyText";
+
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text style={styles.title}>The Game is Over</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>It was clear your number was {props.userNumber}</Text>
+      <BodyText style={styles.title}>The Game is Over</BodyText>
+      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+      <BodyText>It was clear your number was {props.userNumber}</BodyText>
       <View style={styles.buttonContainer}>
         <Button
           color={Colors.contrast}
