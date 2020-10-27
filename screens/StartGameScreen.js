bbac2d0c9fe0cf0f561774e6599cbb8d7567c2cd
manuelsanchez2 <manusansan22@gmail.com>
2020-10-27 14:31:25 +0100

    Apply Dimension Api

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -8,6 +8,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from "react-native";
 
 import Card from "../components/Card";
@@ -127,7 +128,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   buttonContainer: {
-    width: 100,
+    // width: 100,
+    width: Dimensions.get("window").width / 4,
   },
   input: {
     width: 40,
