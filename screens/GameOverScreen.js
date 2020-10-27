c91041e961571ef8e97aaf3c61cff75b6ba7df8d
manuelsanchez2 <manusansan22@gmail.com>
2020-10-27 15:27:11 +0100

    Improve Padding GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -6,6 +6,7 @@ import {
   Button,
   Image,
   Dimensions,
+  ScrollView,
 } from "react-native";
 import Colors from "../constants/colors";
 
@@ -15,40 +16,43 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <TitleText style={styles.title}>The Game is Over</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../assets/success.png")}
-          // source={{
-          //   uri:
-          //     "https://cdn.pixabay.com/photo/2010/11/29/nepal-414_960_720.jpg",
-          // }}
-          style={styles.image}
-          fadeDuration={1000}
-        />
-      </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone just needed{" "}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-          guess the number{" "}
-          <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText style={styles.title}>The Game is Over</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/success.png")}
+            // source={{
+            //   uri:
+            //     "https://cdn.pixabay.com/photo/2010/11/29/nepal-414_960_720.jpg",
+            // }}
+            style={styles.image}
+            fadeDuration={1000}
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone just needed{" "}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{" "}
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
 
-      <View style={styles.buttonContainer}>
-        <MainButton onPress={props.onRestart}>PLAY AGAIN</MainButton>
+        <View style={styles.buttonContainer}>
+          <MainButton onPress={props.onRestart}>PLAY AGAIN</MainButton>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
-    height: "90%",
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
+    paddingVertical: 50,
   },
 
   title: {
@@ -60,13 +64,13 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
   imageContainer: {
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
     borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
-    borderColor: Colors.contrast,
-    marginVertical: Dimensions.get("window").height / 20,
-    width: Dimensions.get("window").width * 0.7,
-    height: Dimensions.get("window").height * 0.7,
+    borderColor: "black",
     overflow: "hidden",
+    marginVertical: Dimensions.get("window").height / 35,
   },
   image: {
     width: "100%",
