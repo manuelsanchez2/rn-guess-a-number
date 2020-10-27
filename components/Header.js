220b3b16eea3c7662d7b7c26efba435310977c81
manuelsanchez2 <manusansan22@gmail.com>
2020-10-27 15:22:16 +0100

    Change header color depending on platform

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import Colors from "../constants/colors";
 
 import TitleText from "./TitleText";
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
       <TitleText>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: "100%",
     height: 90,
     paddingTop: 35,
-    backgroundColor: Colors.contrast,
+
     alignItems: "center",
     justifyContent: "center",
+  },
+  headerIOS: {
+    backgroundColor: "red",
+  },
+  headerAndroid: {
+    backgroundColor: Colors.contrast,
   },
 });
 
